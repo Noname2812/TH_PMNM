f5d3844a6b812b32b712bb9be15dec5e3d1f1f0c
thang <vietthang11012002@gmail.com>
2023-12-19 13:48:58 +0700

    create component footer

---
+++ components/footer.php
@@ -0,0 +1,59 @@
+<footer>
+        <div class="container flex">
+            <div>
+                <div class="res-logo">
+                    <a href=""><img src="./images/res-logo.png" alt="res-logo"></a>
+                    <a href="#">
+                        <h5>Tasty Treat</h5>
+                    </a>
+                </div>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt adipisci ad atque</p>
+            </div>
+            <div>
+                <h5>Delivery Time</h5>
+                <ul>
+                    <li>
+                        <h6>Sunday - Thursday</h6>
+                        <p>10:00am - 11:00pm</p>
+                    </li>
+                    <li>
+                        <h6>Friday - Saturday</h6>
+                        <p>Offday</p>
+                    </li>
+                </ul>
+            </div>
+            <div>
+                <h5>Contact</h5>
+                <ul>
+                    <li>
+                        <h6>Location: HCM</h6>
+                    </li>
+                    <li>
+                        <h6>Phone: 0336796710</h6>
+                    </li>
+                    <li>
+                        <h6>Email: vietthang11012002@gmail.com</h6>
+                    </li>
+                </ul>
+            </div>
+            <div>
+                <h5>Newsletter</h5>
+                <p>Subscribe our newsletter</p>
+                <div class="relative">
+                    <input type="text" placeholder="Enter your email">
+                    <button class="absolute" type="submit">submit</button>
+                </div>
+            </div>
+        </div>
+        <div class="container flex">
+            <div><p><span>Copyright - 2022, website made by Viet Thang. All Rights Reserved.</span></p></div>
+            <div>
+                <ul class="flex">
+                    <li><a href="#"><img src="./images/Socials/facebook.png" alt=""></a></li>
+                    <li><a href="#"><img src="./images/Socials/youtube.png" alt=""></a></li>
+                    <li><a href="#"><img src="./images/Socials/twitter.png" alt=""></a></li>
+                    <li><a href="#"><img src="./images/Socials/instagram.png" alt=""></a></li>
+                </ul>
+            </div>
+        </div>
+</footer>
