565d5fefaf18d740f3b211cf2f5c4651c90d0017
thang <vietthang11012002@gmail.com>
2023-12-19 14:56:13 +0700

    update logic register

--- xuLy/createUser.php
+++ xuLy/createUser.php
@@ -1,7 +1,7 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $user_name = isset($_POST['username']) ? $_POST['username'] : '';
-    $pass_word = isset($_POST['password']) ? $_POST['password'] : '';
+    $username = isset($_POST['username']) ? $_POST['username'] : '';
+    $password = isset($_POST['password']) ? $_POST['password'] : '';
     $confirmPassword = isset($_POST['confirmPassword']) ? $_POST['confirmPassword'] : '';
     $mail = isset($_POST['mail']) ? $_POST['mail'] : '';
     if ($pass_word !== $confirmPassword) {
@@ -13,7 +13,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         require_once "../config.php";
         $pdo = new PDO("mysql:host=$servername;dbname=$dbname", "$username", "$password");
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $sql="insert into user(roleid,username,matkhau,email) values('kh','$user_name','$pass_word','$mail')";
+        $sql="insert into user(roleid,username,matkhau,email) values('kh','$username','$password','$mail')";
         try{
             $pdo->query($sql);
             header("Location: ../login.php?error=3");
